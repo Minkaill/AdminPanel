2b162718648b0e43f5268f7ffca1adab80fa9dd0
Minkaill <mkx.dake@gmail.com>
2024-01-09 05:36:55 +0300

    fix

--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -1,26 +1,16 @@
-import { PlusOutlined, SearchOutlined } from "@ant-design/icons"
 import { TableProducts } from "./components/TableProducts";
 import { useGetProductsQuery } from "services";
 import { columnsProducts } from "data"
 
 import cs from "./products.module.scss"
+import { SearchTable } from "components/SearchTable";
 
 export const Products = () => {
     const { data, isLoading } = useGetProductsQuery()
 
     return (
         <div className={cs.wrapper}>
-            <div className={cs.header}>
-                <div className={cs.live_search}>
-                    <SearchOutlined />
-                    <input type="text" />
-                </div>
-
-                <div className={cs.add}>
-                    <PlusOutlined />
-                    <button>Добавить</button>
-                </div>
-            </div>
+            <SearchTable />
 
             <table className={cs.table}>
                 <thead>
