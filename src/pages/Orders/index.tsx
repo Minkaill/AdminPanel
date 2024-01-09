7cc6154389302ef0fa1f0ba985ceecba5474595e
Minkaill <mkx.dake@gmail.com>
2024-01-09 05:36:34 +0300

    Добавил страницу заказа

--- src/pages/Orders/index.tsx
+++ src/pages/Orders/index.tsx
@@ -1,3 +1,28 @@
+import { useGetProductsQuery } from "services";
+import { columnsProducts } from "data"
+
+import cs from "./orders.module.scss"
+import { SearchTable } from "components/SearchTable";
+import { TableProducts } from "./components/TableOrders";
+
 export const Orders = () => {
-    return <h1>Orders</h1>
+    const { data, isLoading } = useGetProductsQuery()
+
+    return (
+        <div className={cs.wrapper}>
+            <SearchTable />
+
+            <table className={cs.table}>
+                <thead>
+                    <tr >
+                        {columnsProducts.map(({ title, dataIndex }) => (
+                            <th key={dataIndex}>{title}</th>
+                        ))}
+                    </tr>
+                </thead>
+
+                <TableProducts data={data} isLoading={isLoading} />
+            </table>
+        </div>
+    )
 }
